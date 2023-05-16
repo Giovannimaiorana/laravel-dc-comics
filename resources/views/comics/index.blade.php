57dcef338130159fd8a205f6d1a4dede6007082e
Giovanni Maiorana <giovanni.maiorana.14@gmail.com>
2023-05-16 22:14:08 +0200

    aggiunta rotte home e film

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,24 +5,30 @@
 
 
 @section('content')
-@foreach ($comics as $comic)
-<div class="card" style="width: 18rem;">
-    <img src="{{$comic->thumb}}" class="card-img-top">
-    <div class="card-body">
-      <h5 class="card-title">{{$comic->title}}</h5>
-      <p class="card-text">{{$comic->description}}</p>
-    </div>
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item">{{$comic->series}}</li>
-      <li class="list-group-item">{{$comic->sale_date}}</li>
-      <li class="list-group-item">{{$comic->price}}</li>
-    </ul>
-    <div class="card-body">
-      <a href="#" class="btn">Card link</a>
 
-    </div>
-  </div>
-@endforeach
+<div class="d-flex flex-wrap ">
+    @foreach ($comics as $comic)
+    <div class="card mb-3 ms-3" style="width: 18rem;">
+        <img src="{{$comic->thumb}}" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">{{$comic->title}}</h5>
+          <p class="card-text">{{$comic->description}}</p>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">{{$comic->series}}</li>
+          <li class="list-group-item">{{$comic->sale_date}}</li>
+          <li class="list-group-item">{{$comic->price}}</li>
+        </ul>
+        <div class="card-body">
+          <a href="#" class="btn">Card link</a>
+    
+        </div>
+      </div>
+    @endforeach
+
+</div>
+
+
 
 
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,17 +2,17 @@
    
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">DC COMICS</a>
+          <a class="navbar-brand" href="{{ route('home') }}">DC COMICS</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                <a class="nav-link" href="{{ route('comics.index') }}">Film</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
+                <a class="nav-link active" aria-current="page" href="">cc</a>
               </li>
             </ul>
           </div>
